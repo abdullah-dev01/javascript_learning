3548599d2ba71d89f116a8330da5117d8d08908a
abdullah <abdullah.a.dev01@gmail.com>
2025-02-05 21:32:32 +0500

    updated

---
+++ week-1/day_2/variables_in_js.js
@@ -0,0 +1,12 @@
+// 1. Primitive Data Types
+// These are the building blocks of data. They hold a single value and are copied by value.
+
+// Type	Description	Example
+// String	Text enclosed in quotes	"Hello", 'JavaScript'
+// Number	Integers, decimals, or scientific numbers	42, 3.14, 1e5
+// Boolean	Logical true or false	true, false
+// Undefined	Declared but not assigned a value	let x; → x is undefined
+// Null	Intentional absence of value	let y = null;
+// Symbol	Unique, immutable identifier (ES6+)	const id = Symbol("id");
+// BigInt	Extremely large integers (ES2020+)	9007199254740991n
+
