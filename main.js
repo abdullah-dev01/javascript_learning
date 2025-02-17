896b424f0861babe6f8e0e4cfabc5def947dcc7b
abdullah <abdullah.a.dev01@gmail.com>
2025-02-17 22:25:22 +0500

    feature: boolean data type & comparison operators

--- main.js
+++ main.js
@@ -10,3 +10,8 @@ console.log("***");
 console.log("**");
 console.log("***");
 console.log("**");
+
+
+
+
+
--- week-2/day_1.js
+++ week-2/day_1.js
@@ -1,4 +1,4 @@
-day_1
+// day_1
 // Boolean Data Type & Comparison Operators**
 //    - Watch Video #12.
 //    - Practice: Write code to compare different values using 
@@ -6,3 +6,36 @@ day_1
 //    - Example: Compare numbers, strings, and boolean values.
 
 // `==` vs `===`
+
+// `==` checks for value equality (ignoring type)
+// `===` checks for both value and type equality
+
+
+let x = 10;
+let y = "10";
+let z = true;
+
+console.log("See difference between == and ===");
+console.log(x == y); // Output: true
+console.log(x === y); // Output: false
+
+console.log("See difference between != and !==");
+console.log(x != y); // Output: false
+console.log(x !== y); // Output: true
+
+console.log("See difference between >, <, >=, and <=");
+console.log(x > y); // Output: false
+console.log(x < y); // Output: false
+console.log(x >= y); // Output: false
+console.log(x <= y); // Output: false
+
+// these `>`, `<`, `>=`, `<=` does not work with string and boolean
+
+console.log("See difference between >, <, >=, and <=");
+x = 7;
+y = 8;
+
+console.log(x > y); // Output: false
+console.log(x < y); // Output: false
+console.log(x >= y); // Output: false
+console.log(x <= y); // Output: false
